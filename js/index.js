70f7ac1f3c4786a286be2191dcd51cc631b7626f
sujoncham <sojonchambugong@gmail.com>
2022-02-12 00:04:24 +0600

    image added in featured section

--- js/index.js
+++ js/index.js
@@ -14,3 +14,41 @@ const btnId = document.getElementById('btn-id').addEventListener('click', functi
     }
 })
 
+const imageShow = document.getElementById('image-show');
+imageShow.style.cursor = "pointer";
+
+imageShow.addEventListener('click', function(){
+    const image1 = document.getElementById('image1');
+    if(image1 == true){
+        image1.src = "images/images/shopping.png";
+    } else{
+        image1.src = "images/slider/slide1.png";
+    }
+})
+
+
+const imageShow1 = document.getElementById('image-show1');
+imageShow1.style.cursor = "pointer";
+
+imageShow1.addEventListener('click', function(){
+    const image1 = document.getElementById('image1');
+    if(image1 == true){
+        image1.src = "images/images/shopping.png";
+    } else{
+        image1.src = "images/slider/slide2.png";
+    }
+})
+
+const imageShow2 = document.getElementById('image-show2');
+imageShow2.style.cursor = "pointer";
+
+imageShow2.addEventListener('click', function(){
+    const image1 = document.getElementById('image1');
+    if(image1 == true){
+        image1.src = "images/images/shopping.png";
+    } else{
+        image1.src = "images/slider/slide3.png";
+    }
+})
+
+
